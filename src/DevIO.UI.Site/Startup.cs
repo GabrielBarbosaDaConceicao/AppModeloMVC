483031e6afd040bb9febf46d5bed4d14b2c1dd0d
GabrielBarbosaDaConceicao <gabriel.bc.dev@hotmail.com>
2022-04-15 14:01:25 -0300

    Add arquivos front-end

--- src/DevIO.UI.Site/Startup.cs
+++ src/DevIO.UI.Site/Startup.cs
@@ -26,6 +26,8 @@ namespace DevIO.UI.Site
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
